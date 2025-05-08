5e0fdd5da9898fc23ecf15755104e60941f9dc33
Peter Chen <chenwilsoft@msn.com>
2025-05-08 11:17:04 +0800

    Initial Commit

---
+++ include/libwtk3.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------------------------------------------------
+//
+// WTMEC CORPORATION CONFIDENTIAL
+// ________________________________
+//
+// [2025] Wtmec Corporation
+// All Rights Reserved.
+//
+// NOTICE: This is an unpublished work of authorship, which contains trade secrets.
+// Wtmec Corporation owns all rights to this work and intends to maintain it in confidence to
+// preserve its trade secret status. Wtmec Corporation reserves the right, under the copyright
+// laws of the United States or those of any other country that may have jurisdiction, to protect
+// this work as an unpublished work, in the event of an inadvertent or deliberate unauthorized
+// publication. Wtmec Corporation also reserves its rights under all copyright laws to protect
+// this work as a published work, when appropriate. Those having access to this work may not copy
+// it, use it, modify it, or disclose the information contained in it without the written
+// authorization of Wtmec Corporation.
+//
+//---------------------------------------------------------------------------------------------------------------------
+#if !defined(LIBWTK3_H)
+#define LIBWTK3_H (1)
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // LIBWTK3_H
---
+++ src/libwtk3.c
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------------------------------------
+//
+// WTMEC CORPORATION CONFIDENTIAL
+// ________________________________
+//
+// [2025] Wtmec Corporation
+// All Rights Reserved.
+//
+// NOTICE: This is an unpublished work of authorship, which contains trade secrets.
+// Wtmec Corporation owns all rights to this work and intends to maintain it in confidence to
+// preserve its trade secret status. Wtmec Corporation reserves the right, under the copyright
+// laws of the United States or those of any other country that may have jurisdiction, to protect
+// this work as an unpublished work, in the event of an inadvertent or deliberate unauthorized
+// publication. Wtmec Corporation also reserves its rights under all copyright laws to protect
+// this work as a published work, when appropriate. Those having access to this work may not copy
+// it, use it, modify it, or disclose the information contained in it without the written
+// authorization of Wtmec Corporation.
+//
+//---------------------------------------------------------------------------------------------------------------------
+#include <stdint.h>
+#include <stdbool.h>
+#include "libwtk3.h"
+
+int Add(int a, int b) {
+	return a + b;
+}
